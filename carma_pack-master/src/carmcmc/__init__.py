387f8a5a44eba4cdee93ac1cca71043346d8bdb4
Sherry <XinyueSheng@outlook.com>
2021-05-14 22:18:54 +0800

    remove files

--- carma_pack-master/src/carmcmc/__init__.py
+++
@@ -1,4 +0,0 @@
-from _carmcmc import *
-from carma_pack import CarmaModel, CarmaSample, Car1Sample, power_spectrum, carma_variance, \
-    carma_process, get_ar_roots
-from samplers import MCMCSample
